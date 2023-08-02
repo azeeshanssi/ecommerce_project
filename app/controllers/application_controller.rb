9f848d68c43c12eaf982d049d3d81c2ad68d5fc9
alizeeshanssi <azeeshan@ssidecisions.com>
2023-08-02 18:43:21 +0500

    code cleaned

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_type])
-      
+
     end
 end
